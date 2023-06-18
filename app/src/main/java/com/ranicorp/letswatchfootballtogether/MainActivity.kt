88170e67666623e1bbd7090aa099cf0b24506421
geberani <rjqpfksl28@gmail.com>
2023-06-19 01:31:19 +0900

    로그인한 상태인지 확인한 뒤 첫번째 화면으로 이동하도록 처리함

--- app/src/main/java/com/ranicorp/letswatchfootballtogether/MainActivity.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/MainActivity.kt
@@ -2,10 +2,25 @@ package com.ranicorp.letswatchfootballtogether
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import com.ranicorp.letswatchfootballtogether.data.source.repository.UserPreferenceRepository
+import com.ranicorp.letswatchfootballtogether.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val userPreferenceRepository = UserPreferenceRepository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        moveToFirstScreen()
+    }
+
+    private fun moveToFirstScreen() {
+        if (userPreferenceRepository.getFirebaseUserId().isNotEmpty()) {
+            supportFragmentManager.commit {
+                replace(R.id.homeNavHost, HomeFragment())
+            }
+        }
     }
 }
