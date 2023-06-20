7f3817bacc61dfde306cc525d562db08bea35145
geberani <rjqpfksl28@gmail.com>
2023-06-20 23:16:54 +0900

    오타 수정함

--- app/src/main/java/com/ranicorp/letswatchfootballtogether/util/Constants.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/util/Constants.kt
@@ -3,7 +3,7 @@ package com.ranicorp.letswatchfootballtogether.util
 object Constants {
     const val KEY_FIREBASE_UID = "firebase_uid"
     const val KEY_FIREBASE_NICK_NAME = "firebase_nick_name"
-    const val ERROR_EMPTY_NICK_NAME = "error_set_nick_name"
+    const val ERROR_EMPTY_NICK_NAME = "error_empty_nick_name"
     const val ERROR_MAX_LENGTH = "error_max_length"
     const val ERROR_DUPLICATE_NICK_NAME = "error_duplicate_nick_name"
 }
