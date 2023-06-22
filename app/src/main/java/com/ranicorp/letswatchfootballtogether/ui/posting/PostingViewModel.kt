b25488a22ee8fb182e6ed7f7315d05c4ef624e60
geberani <rjqpfksl28@gmail.com>
2023-06-23 00:59:36 +0900

    함수 배치 순서 변경함

--- app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/posting/PostingViewModel.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/posting/PostingViewModel.kt
@@ -42,20 +42,24 @@ class PostingViewModel(
         _imageUriList.value = currentList.plus(uri)
     }
 
+    fun removeImage(uri: Uri) {
+        _imageUriList.value = _imageUriList.value?.minus(uri)
+    }
+
     fun complete() {
         if (isNotValidInfo(imageUriList.value.toString(), R.string.guide_message_set_image)) return
         if (isNotValidInfo(title.value, R.string.guide_message_set_title)) return
         if (isNotValidInfo(location.value, R.string.guide_message_set_location)) return
         if (isNotValidInfo(date.value, R.string.guide_message_set_date)) return
         if (isNotValidInfo(time.value, R.string.guide_message_set_time)) return
-        if (isNotValidInfo(maxParticipants.value, R.string.guide_message_set_max_participants)) return
+        if (isNotValidInfo(
+                maxParticipants.value,
+                R.string.guide_message_set_max_participants
+            )
+        ) return
         if (isNotValidInfo(description.value, R.string.guide_message_set_description)) return
 
         addPost()
-    }
-
-    fun removeImage(uri: Uri) {
-        _imageUriList.value = _imageUriList.value?.minus(uri)
     }
 
     private fun isNotValidInfo(text: String?, messageResId: Int): Boolean {
