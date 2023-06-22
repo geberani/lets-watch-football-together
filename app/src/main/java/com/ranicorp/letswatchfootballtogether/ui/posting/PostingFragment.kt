2fabe686631d941030e3b7f13ac77a2c91941bad
geberani <rjqpfksl28@gmail.com>
2023-06-22 21:50:23 +0900

    MVVM 패턴, data binding 사용하여 글쓰는 화면 구현함

---
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/posting/PostingFragment.kt
@@ -0,0 +1,111 @@
+package com.ranicorp.letswatchfootballtogether.ui.posting
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.ConcatAdapter
+import com.google.firebase.storage.FirebaseStorage
+import com.ranicorp.letswatchfootballtogether.FootballApplication
+import com.ranicorp.letswatchfootballtogether.R
+import com.ranicorp.letswatchfootballtogether.data.source.remote.RemoteDataSource
+import com.ranicorp.letswatchfootballtogether.data.source.repository.PostRepository
+import com.ranicorp.letswatchfootballtogether.data.source.repository.UserPreferenceRepository
+import com.ranicorp.letswatchfootballtogether.databinding.FragmentPostingBinding
+import com.ranicorp.letswatchfootballtogether.ui.common.ProgressDialogFragment
+
+class PostingFragment : Fragment(), DeleteClickListener, HeaderClickListener {
+
+    private var _binding: FragmentPostingBinding? = null
+    private val binding get() = _binding!!
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            if (uri != null) {
+                viewModel.addImage(uri)
+                updateAdapterData()
+            }
+        }
+    private val attachedImageAdapter = ImageAdapter(this)
+    private val attachedImageHeaderAdapter = HeaderAdapter(this)
+    private val viewModel by viewModels<PostingViewModel> {
+        PostingViewModel.provideFactory(
+            PostRepository(RemoteDataSource(FootballApplication.appContainer.provideApiClient())),
+            UserPreferenceRepository(),
+            FirebaseStorage.getInstance()
+        )
+    }
+    private val progressDialog = ProgressDialogFragment()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPostingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        setLayout()
+    }
+
+    private fun updateAdapterData() {
+        attachedImageAdapter.submitList(viewModel.imageUriList.value)
+        attachedImageHeaderAdapter.submitList(
+            getString(
+                R.string.number_of_image_displayed,
+                viewModel.imageUriList.value?.size
+            )
+        )
+        attachedImageHeaderAdapter.notifyDataSetChanged()
+    }
+
+    private fun setLayout() {
+        binding.imageRecyclerView.adapter =
+            ConcatAdapter(attachedImageHeaderAdapter, attachedImageAdapter)
+        attachedImageHeaderAdapter.submitList(getString(R.string.number_of_image_displayed, 0))
+        viewModel.errorMsgResId.observe(viewLifecycleOwner) {
+            Toast.makeText(
+                context,
+                getString(it),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        binding.btnAddPost.setOnClickListener {
+            viewModel.addPost()
+        }
+        viewModel.isAdded.observe(viewLifecycleOwner) {
+            if (it == true) {
+                progressDialog.show(requireActivity().supportFragmentManager, null)
+            } else {
+                progressDialog.dismiss()
+            }
+        }
+    }
+
+    override fun onDeleteClick(uri: String) {
+        viewModel.removeImage(uri.toUri())
+        updateAdapterData()
+    }
+
+    override fun onHeaderClick() {
+        if (viewModel.imageUriList.value?.size == 10) {
+            Toast.makeText(
+                context,
+                getString(R.string.guide_message_limit_max_images),
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            getContent.launch("image/*")
+        }
+    }
+}
---
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/posting/PostingViewModel.kt
@@ -0,0 +1,115 @@
+package com.ranicorp.letswatchfootballtogether.ui.posting
+
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.google.firebase.storage.FirebaseStorage
+import com.ranicorp.letswatchfootballtogether.R
+import com.ranicorp.letswatchfootballtogether.data.model.Post
+import com.ranicorp.letswatchfootballtogether.data.source.repository.PostRepository
+import com.ranicorp.letswatchfootballtogether.data.source.repository.UserPreferenceRepository
+import com.ranicorp.letswatchfootballtogether.util.DateFormatText.getCurrentDateString
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class PostingViewModel(
+    private val postRepository: PostRepository,
+    private val userPreferenceRepository: UserPreferenceRepository,
+    private val firebaseStorage: FirebaseStorage
+) : ViewModel() {
+
+    val title = MutableLiveData<String>()
+    val location = MutableLiveData<String>()
+    val date = MutableLiveData<String>()
+    val time = MutableLiveData<String>()
+    val maxParticipants = MutableLiveData<String>()
+    val description = MutableLiveData<String>()
+    private val _errorMsgResId = MutableLiveData<Int>()
+    val errorMsgResId = _errorMsgResId
+    private val _imageUriList: MutableLiveData<List<Uri>> = MutableLiveData()
+    val imageUriList: LiveData<List<Uri>> = _imageUriList
+    private val _isAdded: MutableLiveData<Boolean> = MutableLiveData()
+    val isAdded: LiveData<Boolean> = _isAdded
+    private val userUid = userPreferenceRepository.getUserUid()
+
+    fun addImage(uri: Uri) {
+        val currentList = _imageUriList.value ?: emptyList()
+        _imageUriList.value = currentList.plus(uri)
+    }
+
+    fun addPost() {
+        if (imageUriList.value.isNullOrEmpty()) {
+            _errorMsgResId.value = R.string.guide_message_set_image
+            return
+        }
+        if (isNotValidInfo(title.value, R.string.guide_message_set_title)) return
+        if (isNotValidInfo(location.value, R.string.guide_message_set_location)) return
+        if (isNotValidInfo(date.value, R.string.guide_message_set_date)) return
+        if (isNotValidInfo(time.value, R.string.guide_message_set_time)) return
+        if (maxParticipants.value.isNullOrBlank()) {
+            _errorMsgResId.value = R.string.guide_message_set_max_participants
+            return
+        }
+        if (isNotValidInfo(description.value, R.string.guide_message_set_description)) return
+
+        viewModelScope.launch {
+            _isAdded.value = true
+            val imageLocations = addImageToStorage(_imageUriList.value?.toList() ?: emptyList())
+            val post = Post(
+                userUid + getCurrentDateString(),
+                userUid,
+                getCurrentDateString(),
+                title.value!!,
+                location.value!!,
+                date.value!!,
+                time.value!!,
+                maxParticipants.value!!.toInt(),
+                description.value!!,
+                imageLocations,
+                mutableListOf(userUid)
+            )
+            if (postRepository.addPost(post).isSuccessful) {
+                _isAdded.value = false
+            }
+            //TODO 해당 게시물 채팅방 생성, User의 ParticipatingEvent List에 위 이벤트 추가
+        }
+    }
+
+    fun removeImage(uri: Uri) {
+        _imageUriList.value = _imageUriList.value?.minus(uri)
+    }
+
+    private fun isNotValidInfo(text: String?, messageResId: Int): Boolean {
+        if (text.isNullOrBlank()) {
+            _errorMsgResId.value = messageResId
+            return true
+        }
+        return false
+    }
+
+    private suspend fun addImageToStorage(imageList: List<Uri>): List<String> = coroutineScope {
+        imageList.map { imageUri ->
+            val location = "images/${imageUri}"
+            val imageRef = firebaseStorage.getReference(location)
+            imageRef.putFile(imageUri).await()
+            location
+        }
+    }
+
+    companion object {
+        fun provideFactory(
+            postRepository: PostRepository,
+            userPreferenceRepository: UserPreferenceRepository,
+            firebaseStorage: FirebaseStorage
+        ) = viewModelFactory {
+            initializer {
+                PostingViewModel(postRepository, userPreferenceRepository, firebaseStorage)
+            }
+        }
+    }
+}
