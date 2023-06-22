1fd054f0c585b25fea0f6436d5803dd879b0846b
geberani <rjqpfksl28@gmail.com>
2023-06-22 21:46:54 +0900

    이미지 모양에 따른 Binding Adapter 구현하여 적용함

--- app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/databindings/ImageViewBindings.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/databindings/ImageViewBindings.kt
@@ -4,13 +4,26 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 import com.ranicorp.letswatchfootballtogether.R
 
-@BindingAdapter("imageUri")
-fun loadImage(view: ImageView, uri: String?) {
+@BindingAdapter("circleCropImageUri")
+fun loadCircleCropImage(view: ImageView, uri: String?) {
     if (uri != null) {
         view.load(uri) {
             transformations(CircleCropTransformation())
+            error(R.drawable.ic_image_not_supported)
+        }
+    } else {
+        //이미지를 선택하지 않았을 때에 대한 처리
+    }
+}
+
+@BindingAdapter("squareCropImageUri")
+fun loadSquareCropImage(view: ImageView, uri: String?) {
+    if (uri != null) {
+        view.load(uri) {
+            transformations(RoundedCornersTransformation(8f))
             error(R.drawable.ic_image_not_supported)
         }
     } else {
