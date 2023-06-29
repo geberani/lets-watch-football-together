b9e288169bd160787d43d1e15d43df4e9cd2c9a5
geberani <rjqpfksl28@gmail.com>
2023-06-29 18:38:27 +0900

    날짜 형식에 맞도록 수정함

--- app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/home/AllPostAdapter.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/home/AllPostAdapter.kt
@@ -8,6 +8,7 @@ import com.google.firebase.storage.FirebaseStorage
 import com.ranicorp.letswatchfootballtogether.data.model.Post
 import com.ranicorp.letswatchfootballtogether.databinding.ItemAllPostBinding
 import com.ranicorp.letswatchfootballtogether.ui.common.PostClickListener
+import com.ranicorp.letswatchfootballtogether.util.DateFormatText
 
 class AllPostAdapter(private val clickListener: PostClickListener) :
     ListAdapter<Post, AllPostAdapter.AllPostItemViewHolder>(PostDiffCallback()) {
@@ -25,7 +26,8 @@ class AllPostAdapter(private val clickListener: PostClickListener) :
 
         fun bind(item: Post, clickListener: PostClickListener) {
             binding.post = item
-            binding.dateTimeInfoLayout.setInfo(item.date + " " + item.time)
+            val date = DateFormatText.longToDisplayDate(item.createdTimeMillis)
+            binding.dateTimeInfoLayout.setInfo(date + " " + item.time)
             binding.locationInfoLayout.setInfo(item.location)
             binding.clickListener = clickListener
             binding.postUid = item.postUid
--- app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/home/PopularPostAdapter.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/home/PopularPostAdapter.kt
@@ -8,6 +8,7 @@ import com.google.firebase.storage.FirebaseStorage
 import com.ranicorp.letswatchfootballtogether.data.model.Post
 import com.ranicorp.letswatchfootballtogether.databinding.ItemPopularPostBinding
 import com.ranicorp.letswatchfootballtogether.ui.common.PostClickListener
+import com.ranicorp.letswatchfootballtogether.util.DateFormatText
 
 class PopularPostAdapter(private val clickListener: PostClickListener) :
     ListAdapter<Post, PopularPostAdapter.PopularPostItemViewHolder>(PostDiffCallback()) {
@@ -32,7 +33,8 @@ class PopularPostAdapter(private val clickListener: PostClickListener) :
             imageRef.downloadUrl.addOnSuccessListener {
                 binding.previewImageUri = it.toString()
             }
-            binding.dateTimeInfoLayout.setInfo(item.date + " " + item.time)
+            val date = DateFormatText.longToDisplayDate(item.createdTimeMillis)
+            binding.dateTimeInfoLayout.setInfo(date + " " + item.time)
             binding.locationInfoLayout.setInfo(item.location)
         }
 
