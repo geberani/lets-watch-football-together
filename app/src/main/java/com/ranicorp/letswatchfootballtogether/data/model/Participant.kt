2dbc1669a3ba8f2a745b93b274348c41799c5121
geberani <rjqpfksl28@gmail.com>
2023-07-01 00:32:15 +0900

    이미지 ViewPager, 참여 인원 RecyclerView Adapter 구현함

---
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/data/model/Participant.kt
@@ -0,0 +1,6 @@
+package com.ranicorp.letswatchfootballtogether.data.model
+
+data class Participant(
+    val nickName: String,
+    val profileUri: String
+)
--- app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/detail/DetailFragment.kt
@@ -1,9 +1,40 @@
 package com.ranicorp.letswatchfootballtogether.ui.detail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import com.ranicorp.letswatchfootballtogether.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
 
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvParticipantsList.adapter = ParticipantsAdapter()
+        binding.imageViewPager.adapter = BannerAdapter()
+        TabLayoutMediator(binding.indicator, binding.imageViewPager) { tab, position ->
+
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
