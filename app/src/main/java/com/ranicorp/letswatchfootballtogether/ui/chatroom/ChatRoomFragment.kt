b47dc1e2411248f7690a232692a3b9dc290fcd04
geberani <rjqpfksl28@gmail.com>
2023-07-04 15:17:27 +0900

    ChatFragment -> ChatRoomFragment로 이름 변경함

---
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/ui/chatroom/ChatRoomFragment.kt
@@ -0,0 +1,11 @@
+package com.ranicorp.letswatchfootballtogether.ui.chatroom
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ChatRoomFragment : Fragment() {
+/*
+    1. EditText에 메세지를 작성한다.
+  */
+}
