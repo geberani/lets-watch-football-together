3a6dee079c1236a3688ac4278a826b7a66af579f
geberani <rjqpfksl28@gmail.com>
2023-07-06 01:22:25 +0900

    Bottom Navigation을 LauncherActivity에 추가하고 Jetpack navigation과 연결함

--- app/src/main/java/com/ranicorp/letswatchfootballtogether/LauncherActivity.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/LauncherActivity.kt
@@ -2,8 +2,12 @@ package com.ranicorp.letswatchfootballtogether
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import androidx.fragment.app.commit
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ranicorp.letswatchfootballtogether.data.source.repository.UserPreferenceRepository
+import com.ranicorp.letswatchfootballtogether.databinding.ActivityMainBinding
 import com.ranicorp.letswatchfootballtogether.ui.home.HomeFragment
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -13,11 +17,33 @@ class LauncherActivity : AppCompatActivity() {
 
     @Inject
     lateinit var userPreferenceRepository: UserPreferenceRepository
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         moveToFirstScreen()
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        val navController = supportFragmentManager.findFragmentById(R.id.homeNavHost)?.findNavController()
+        navController?.let {
+            binding.homeBottomNavigationView.setupWithNavController(it)
+        }
+
+        navController?.addOnDestinationChangedListener { _, destination, _ ->
+            val bottomNavigation = binding.homeBottomNavigationView
+            when (destination.id) {
+                R.id.homeFragment, R.id.profileFragment -> {
+                    bottomNavigation.isVisible = true
+                }
+                else -> {
+                    bottomNavigation.isVisible = false
+                }
+            }
+        }
     }
 
     private fun moveToFirstScreen() {
