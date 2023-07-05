6b1a1ecd01b3a3a5ff0364a733150055b782797f
geberani <rjqpfksl28@gmail.com>
2023-07-05 16:57:32 +0900

    Chat -> Message Data Class 이름 변경함

--- app/src/main/java/com/ranicorp/letswatchfootballtogether/data/model/Message.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/data/model/Message.kt
@@ -1,5 +1,8 @@
 package com.ranicorp.letswatchfootballtogether.data.model
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class Message(
     val senderUid: String,
     val senderNickname: String,
