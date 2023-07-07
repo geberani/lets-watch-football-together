5cd3e079af0b0c81d4a5b2fc0bbc148b9305766d
geberani <rjqpfksl28@gmail.com>
2023-07-08 00:46:59 +0900

    Navigation 세팅 함수 설정함

--- app/src/main/java/com/ranicorp/letswatchfootballtogether/LauncherActivity.kt
+++ app/src/main/java/com/ranicorp/letswatchfootballtogether/LauncherActivity.kt
@@ -3,12 +3,11 @@ package com.ranicorp.letswatchfootballtogether
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.isVisible
-import androidx.fragment.app.commit
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.ranicorp.letswatchfootballtogether.data.source.repository.UserPreferenceRepository
 import com.ranicorp.letswatchfootballtogether.databinding.ActivityMainBinding
-import com.ranicorp.letswatchfootballtogether.ui.home.HomeFragment
+import com.ranicorp.letswatchfootballtogether.ui.home.HomeFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -23,33 +22,30 @@ class LauncherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        moveToFirstScreen()
-        setBottomNavigation()
+        setNavigation()
     }
 
-    private fun setBottomNavigation() {
-        val navController = supportFragmentManager.findFragmentById(R.id.homeNavHost)?.findNavController()
+    private fun setNavigation() {
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.homeNavHost)?.findNavController()
         navController?.let {
             binding.homeBottomNavigationView.setupWithNavController(it)
+        }
+
+        if (userPreferenceRepository.getUserUid().isEmpty()) {
+            val action = HomeFragmentDirections.actionHomeFragmentToSignInFragment()
+            navController?.navigate(action)
         }
 
         navController?.addOnDestinationChangedListener { _, destination, _ ->
             val bottomNavigation = binding.homeBottomNavigationView
             when (destination.id) {
-                R.id.homeFragment, R.id.profileFragment -> {
+                R.id.homeFragment, R.id.profileFragment, R.id.chatRoomListFragment -> {
                     bottomNavigation.isVisible = true
                 }
                 else -> {
                     bottomNavigation.isVisible = false
                 }
-            }
-        }
-    }
-
-    private fun moveToFirstScreen() {
-        if (userPreferenceRepository.getUserUid().isNotEmpty()) {
-            supportFragmentManager.commit {
-                replace(R.id.homeNavHost, HomeFragment())
             }
         }
     }
